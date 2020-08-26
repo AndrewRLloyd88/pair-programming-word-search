063c757bbf23a1f3af97af6858afe7e883ef0fd0
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2020-08-26 23:55:17 +0000

    work with Andre Iskandar and Carl comtois pair programming for wordsearch.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,14 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+	const horizontalJoin = letters.map(ls => ls.join(''));
+	for (let l of horizontalJoin) {
+		if (l.includes(word)) {
+			return true;
+		}
+	}
+	return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
+
+// Check to find the word horizontally and vertically
+// Return true if the word is found, and return false if the word is not found
